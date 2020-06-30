9e693226d81cb4a2775bc2888a99963581a4e8e0
Jzavier Timm <jztimm@gmail.com>
2020-06-30 02:23:56 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,32 @@
 # Write your methods here
+
+def loop_message_five_times(array)
+  counter = 0
+  while counter < array.length do
+    puts "Hi."
+  end
+end
+
+
+def loop_message_n_times(array, n)
+  # code here
+end
+
+
+def output_array(array)
+  # code here
+end
+
+
+def return_string_array
+  # code here
+end
+
+
+
+
+
+
+
+
+
