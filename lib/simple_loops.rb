29726e031e511d8de7b830de606b6842d5d57574
Jzavier Timm <jztimm@gmail.com>
2020-06-30 02:30:29 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -11,8 +11,12 @@ def loop_message_five_times(message)
 end
 
 
-def loop_message_n_times(array, n)
-  # code here
+def loop_message_n_times(message, n)
+  count = 0
+  while count < n do
+    puts message
+    count += 1
+  end
 end
 
 
